68c5e29112dc113cf217fb394c25d3464293d281
rrcharan <34278782+rrcharan@users.noreply.github.com>
2017-12-05 21:45:42 +0530

    Update app.js
    updated DB variable

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://skbtech:skbtech@ds123926.mlab.com:23926/locallibrary';
+var mongoDB = process.env.MONGODB_URI || 'mongodb://skbtech:skbtech@ds123926.mlab.com:23926/locallibrary';
 mongoose.connect(mongoDB, {
   useMongoClient: true
 });
